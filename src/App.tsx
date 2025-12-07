822b4917985f746eace43ddca0e159b7db86b113
Gabriel Araujo <gabrielaaraujodev@gmail.com>
2025-12-07 18:50:31 -0300

    ✨ feat: Refactor Zustand state management to enable individual card state manipulation and add functionality to create multiple cards.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { UnitMeasureConversor } from "./pages/UnitMeasureConversor";
 import { Layout } from "./pages/Layout";
-import { useIsDevMode } from "./store/hooks";
-import { useEffect } from "react";
 import { TechnicalDatasheetPage } from "./pages/TechnicalDatasheetPage";
 import { RestoreFromURL } from "./design_system/RestoreFromUrl";
-
-
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<UnitMeasureConversor />} />
-        <Route path="technical-datasheet" element={<TechnicalDatasheetPage />} />
-      </Route>
-    </Routes>
-  );
-};
+import { UnitMeasureConversorManager } from "./pages/UnitMeasureConversorManager";
 
 export default function App() {
-  const devMode = useIsDevMode();
-
-  useEffect(() => {
-    if (window.location.href.includes("localhost"))
-      devMode.setIsDevMode(true);
-  }, []);
 
   return (
     <HashRouter basename="/">
-      <RestoreFromURL/>
-      <AppRoutes />
+      <RestoreFromURL />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<UnitMeasureConversorManager />} />
+
+          <Route path="technical-datasheet" element={<TechnicalDatasheetPage />} />
+        </Route>
+      </Routes>
+
     </HashRouter>
   );
 }
