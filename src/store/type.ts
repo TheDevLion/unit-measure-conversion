e99f7efcb5da7187d9498c36db96363260c92bc7
Rodrigo Cezar Leão <rodrigoczleo@yahoo.com.br>
2025-12-16 15:52:54 -0500

    [Gabriel] Fix/precision validation (#6)
    * 🐞 fix: hide 'Random' units in selector and clear selected units with the clear button
    
    - Removed 'Random' units from the unit selector.
    
    - Updated the clear button to also remove currently selected units.
    
    * 🐞 fix: Avoid negative output precision
    
    * 🐞 fix: Fix prefer-const error in UnitMeasureConversor
    
    * sdfasdsa
    
    ---------
    
    Co-authored-by: Gabriel Araujo <gabrielaaraujodev@gmail.com>

--- src/store/type.ts
+++ src/store/type.ts
@@ -2,18 +2,21 @@ export type MeasureConversionState = {
     input: {
         value: number | string;
         unit: string;
-    }
+    };
     output: {
+        value: number | string;
         unit: string;
         precision: number;
-    },
+    };
     isDevMode: boolean;
-}
+    isSwitching: boolean;
+};
 
 export type MeasureConversationActions = {
     setInput: (input: Partial<MeasureConversionState['input']>) => void;
     setOutput: (output: Partial<MeasureConversionState['output']>) => void;
     setIsDevMode: (isDevMode: boolean) => void;
-}
+    setIsSwitching: (value: boolean) => void;
+};
 
 export type MeasureConversionStore = MeasureConversionState & MeasureConversationActions;
